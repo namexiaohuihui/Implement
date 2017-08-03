a4432846e462069f9803ccd3be49eeeaa802cdc6
Administrator <704866169@qq.com>
2017-08-03 18:22:34 +0800

    唉

--- src/main/java/wap/business/example/innose/Information.java
+++ src/main/java/wap/business/example/innose/Information.java
@@ -20,38 +20,46 @@ import static java.lang.Thread.sleep;
  * Created by Administrator on 2016/9/22.
  */
 public class Information {
-    String listBar[] ;
-    String url[] = new WapUrl().getShopManagementFamily();
-    private WebDriver driver = FoxDriver.getFoxDriver();
+    //主菜单:
+    public String mainHome = "家庭管理";
+    //子菜单
+    public String listBar[] ;
+    //子菜单链接
+    public String url[] = new WapUrl().getShopManagementFamily();
+    //浏览器对象
+    public WebDriver driver = FoxDriver.getFoxDriver();
+    //按钮点击对象
+    public Preservation preservation = new Preservation();
 
-    public void getStore() throws InterruptedException {
+    public void store() throws InterruptedException {
 
 //      此处实现的是触发link1(触发一级目录)
-        new Preservation().buttonLinkText(listBar[0]);
 
-//        点击店铺管理下面的子目录实现店铺信息、店铺公告、店铺设置、字体设置的数据设置。
         try {
-            getManagement();
+            management();
+            preservation.buttonLinkText(mainHome);
         } catch (SQLException e) {
+            e.printStackTrace();
+        }catch (InterruptedException e){
             e.printStackTrace();
         }
 
     }
 
     //    负责下的子目录切换。
-    private void getManagement() throws InterruptedException, SQLException {
-        for (int i = 2; i <listBar.length; i++) {
+    private void management() throws InterruptedException, SQLException {
+        for (int i = 0; i <listBar.length; i++) {
             switch (i) {
                 case 0:
-                    driver.findElement(By.linkText(listBar[i])).click();
-                    new StoreInformation().getInformation(url[i]);
+                    preservation.buttonLinkText(listBar[i]);
+                    new StoreInformation().informationStore();
                     break;
                 case 1:
-                    driver.findElement(By.linkText(listBar[i])).click();
+                    preservation.buttonLinkText(listBar[i]);
                     new ShopNotices().getAnnouncement(url[i]);
                     break;
                 case 2:
-                    driver.findElement(By.linkText(listBar[i])).click();
+                    preservation.buttonLinkText(listBar[i]);
                     new StoreSettings().getSetting(url[i]);
                     break;
                 default:
