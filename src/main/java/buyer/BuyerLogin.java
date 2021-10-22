9d2e50503e6157f79ba17d8c68c655c5a7fca6a8
dingdong <704866169@qq.com>
2021-10-22 10:31:23 +0800

    修改

--- src/main/java/buyer/BuyerLogin.java
+++ src/main/java/buyer/BuyerLogin.java
@@ -8,5 +8,9 @@ package buyer;
  * @project : Implement
  */
 public class BuyerLogin {
-
+    public static void main(String[] args) {
+        String phone="19163856601";
+        int phoneLen=phone.length();
+        System.out.println(phoneLen);
+    }
 }
